ca705b02949a2101e315f3a21a495f2f8799b77b
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-05-25 23:46:50 -0600

    feat: Proyecto 7 Completado

--- mi-exploracion-espacial/planetas.js
+++ mi-exploracion-espacial/planetas.js
@@ -2,18 +2,33 @@ const planetas = [
   {
     nombre: "Titán",
     descripcion: "La luna más grande de Saturno, con lagos de metano.",
-    descubiertoEn: "1655"
+    descubiertoEn: "1655",
+    radioDelPlaneta: "2575 km"
   },
   {
     nombre: "Próxima Centauri b",
     descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
-    descubiertoEn: "2016"
+    descubiertoEn: "2016",
+    radioDelPlaneta: "6569 km"
   },
   // ¡Añade más planetas!
   {
     nombre: "Mercurio",
     descripcion:"Planeta más cercano al sol",
-    descubiertoEn: "3000 a.c"
+    descubiertoEn: "3000 a.c",
+    radioDelPlaneta: "2440 km"
+  },
+  {
+    nombre: "Venus",
+    descripcion: "Planeta más caliente del sistema solar",
+    descubiertoEn: "600 a.c. (approximadamente)",
+    radioDelPlaneta: "6052 km"
+  },
+  {
+    nombre: "Marte",
+    descripcion: "Planeta rojo",
+    descubiertoEn: "1610",
+    radioDelPlaneta: "3390 km"
   }
 ];
 
