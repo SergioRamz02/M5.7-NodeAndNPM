0ee20594c4fa28d26d3668ca0dfc16dc0fe0267f
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-05-25 23:26:13 -0600

    feat: Inicializando proyecto

---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,20 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  // ¡Añade más planetas!
+  {
+    nombre: "Mercurio",
+    descripcion:"Planeta más cercano al sol",
+    descubiertoEn: "3000 a.c"
+  }
+];
+
+module.exports = planetas;
